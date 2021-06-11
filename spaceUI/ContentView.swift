2bec15b7e34233fb3040865ef94ffa68778597f6
Your Name <you@example.com>
2021-06-11 16:17:19 +0800

    Initial Commit

---
+++ spaceUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  spaceUI
+//
+//  Created by Apple on 11/6/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ spaceUI/spaceUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  spaceUIApp.swift
+//  spaceUI
+//
+//  Created by Apple on 11/6/2021.
+//
+
+import SwiftUI
+
+@main
+struct spaceUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
